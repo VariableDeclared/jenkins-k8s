551f4ad3e9b2dc4abbcaf6adfda767f3e3c4eb41
Peter De Sousa <peter.de.sousa@canonical.com>
2023-09-26 17:03:48 +0100

    Git SCM and parameterised jenkins pipeline
    * Added parameters
    * Added git checkout
    
    TODO:
    - Working with git directories cleanly
    - Plumb in terraform files
    - Plumb in juju bootstrap

--- pipeline.groovy
+++ pipeline.groovy
@@ -9,27 +9,35 @@ credentials:
 
 final CFG_PATH = './credentials.yaml'
 
-
-
 import groovy.text.StreamingTemplateEngine
 
 def renderTemplate(input, variables) {
-  def engine = new StreamingTemplateEngine()
-  return engine.createTemplate(input).make(variables).toString()
+    def engine = new StreamingTemplateEngine()
+    return engine.createTemplate(input).make(variables).toString()
 }
 
 pipeline {
     agent any
+    parameters {
+        string(name: 'UBUNTU_IMAGE_ID', defaultValue: 'none', description: 'Openstack image ID')
+        string(name: 'OS_SERIES', defaultValue: 'jammy', description: 'Ubuntu OS Series')
+        string(name: 'OPENSTACK_REGION', defaultValue: 'az1', description: 'Openstack regions')
+        string(name: 'OPENSTACK_URL', defaultValue: 'https://localhost:9000/auth/v3', description: 'Keystone URL')
+        string(name: 'GIT_REPO', defaultValue: 'https://github.com/variabledeclared/blah', description: 'Keystone URL')
+    }
     stages {
         stage('Bootstrap juju') {
             environment {
-                JUJU_ACCESS_KEY = credentials("aws-access-key")
-                JUJU_ACCESS_SECRET = credentials("aws-access-secret")
+                NOVA_RC_BASE64 = credentials('jenkins-novarc')
             }
             steps {
                 sh 'sudo snap install juju || true'
-                sh 'mkdir -p /home/jenkins/.local/share || true' 
-        
+                checkout scmGit(branches: [[name: 'main']],
+                userRemoteConfigs: [
+                    [url: params.GIT_REPO]
+                ])
+                sh 'mkdir -p /home/jenkins/.local/share || true'
+
                 writeFile file: CFG_PATH, text: renderTemplate(JUJU_CREDENTIAL_TEMPLATE, ['access_key': JUJU_ACCESS_KEY, 'access_secret': JUJU_ACCESS_SECRET])
                 sh 'juju add-credential --client aws -f ./credentials.yaml'
                 sh 'juju bootstrap aws/eu-west-1'
