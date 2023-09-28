687bd9e589a3171c415f7abaca2907606d07f2e5
Peter De Sousa <peter.de.sousa@canonical.com>
2023-09-28 14:43:14 +0100

    Fix terraform application & bootstrapping
    * Terraform create the environment
    * bootstrap juju on this environment
    
    TODO: Rendering the CA needs fixing

--- pipeline.groovy
+++ pipeline.groovy
@@ -16,14 +16,22 @@ def renderTemplate(input, variables) {
     return engine.createTemplate(input).make(variables).toString()
 }
 
+def base64Decode(encodedString){
+    byte[] decoded = encodedString.decodeBase64()
+    String decode = new String(decoded)
+    return decode
+}
+
 pipeline {
     agent any
     parameters {
         string(name: 'UBUNTU_IMAGE_ID', defaultValue: 'none', description: 'Openstack image ID')
         string(name: 'OS_SERIES', defaultValue: 'jammy', description: 'Ubuntu OS Series')
-        string(name: 'OPENSTACK_REGION', defaultValue: 'az1', description: 'Openstack regions')
-        string(name: 'OPENSTACK_URL', defaultValue: 'https://localhost:9000/auth/v3', description: 'Keystone URL')
-        string(name: 'GIT_REPO', defaultValue: 'https://github.com/variabledeclared/blah', description: 'Keystone URL')
+        string(name: 'OPENSTACK_REGION', defaultValue: 'RegionOne', description: 'Openstack regions')
+        string(name: 'OPENSTACK_URL', defaultValue: 'https://keystone.orange.box:5000/v3', description: 'Keystone URL')
+        string(name: 'GIT_REPO', defaultValue: 'https://github.com/VariableDeclared/jenkins-terraform', description: 'Git URL')
+        string(name: 'O7K_USER_NAME', defaultValue: 'Alice', description: 'O7K User to be created')
+        string(name: 'O7K_PASSWD', defaultValue: 'Alice', description: 'O7K User to be created')
     }
     stages {
         stage('Git Checkout') {
@@ -32,7 +40,7 @@ pipeline {
                     userRemoteConfigs: [
                         [url: params.GIT_REPO]
                     ],
-                    extensions: [[$class: 'RelativeTargetDirectory', 
+                    extensions: [[$class: 'RelativeTargetDirectory',
                         relativeTargetDir: 'jenkins-terraform']]
                     )
             }
@@ -44,25 +52,44 @@ pipeline {
             steps {
                 dir('jenkins-terraform') {
                     sh 'sudo snap install terraform --classic || true'
-                    sh 'terraform init || true'
+                    sh 'terraform init -upgrade || true'
                     writeFile file: './id_rsa.pub', text: SSH_PUBLIC_KEY
+                    sh 'for domain in  Engineering Support Administration; \
+                    do openstack domain set --disable $domain; done'
+                    sh 'terraform apply -auto-approve -destroy'
                     sh 'terraform apply -auto-approve'
-                    
-                    // sh './scripts/generate-jujumetadata.sh'
+                // sh './scripts/generate-jujumetadata.sh'
                 }
             }
         }
         stage('Bootstrap juju') {
             environment {
                 NOVA_RC_BASE64 = credentials('jenkins-novarc')
+                OPENSTACK_CA_CERT = credentials('o7k-ca-cert')
             }
             steps {
-                sh 'sudo snap install juju || true'
-                sh 'mkdir -p /home/jenkins/.local/share || true'
+                dir('jenkins-terraform') {
+                    sh 'sudo snap install juju || true'
+                    sh "mkdir -p ${HOME}/.local/share || true"
 
-                writeFile file: CFG_PATH, text: renderTemplate(JUJU_CREDENTIAL_TEMPLATE, ['access_key': JUJU_ACCESS_KEY, 'access_secret': JUJU_ACCESS_SECRET])
-                sh 'juju add-credential --client aws -f ./credentials.yaml'
-                sh 'juju bootstrap aws/eu-west-1'
+                    sh "./scripts/setup-juju-o7k-cloud.sh ${params.OPENSTACK_URL} \
+                     ${OPENSTACK_CA_CERT} ${params.O7K_PASSWD} \
+                    Engineering Development ${params.O7K_USER_NAME}"
+                    // writeFile file: CFG_PATH, text:
+                    // renderTemplate(JUJU_CREDENTIAL_TEMPLATE,
+                    // ['access_key': JUJU_ACCESS_KEY, 'access_secret': 
+                    // JUJU_ACCESS_SECRET])
+                    writeFile file: "${WORKSPACE}/novarc", text: base64Decode(NOVA_RC_BASE64)
+                    sh "./scripts/generate-jujumetadata.sh \
+                    ${WORKSPACE}/novarc ${params.UBUNTU_IMAGE_ID} \
+                    ${params.OS_SERIES} ${params.OPENSTACK_REGION} \
+                    ${params.OPENSTACK_URL}"
+                    sh 'juju bootstrap openstack_cloud \
+                     --metadata-source ~/simplestreams \
+                     --model-default="network=Frontend" \
+                     --model-default=external-network="ext-net"\
+                     --bootstrap-constraints="allocate-public-ip=true"'
+                }
             }
         }
         stage('Deploy Kubernetes') {
@@ -70,6 +97,17 @@ pipeline {
                 sh 'juju add-model k8s-jenkins'
                 sh 'juju deploy charmed-kubernetes'
             }
+        }
+    }
+    post {
+        // Clean after build
+        always {
+            cleanWs(cleanWhenNotBuilt: false,
+                    deleteDirs: true,
+                    disableDeferredWipeout: true,
+                    notFailBuild: true,
+                    patterns: [[pattern: '.gitignore', type: 'INCLUDE'],
+                               [pattern: '.propsfile', type: 'EXCLUDE']])
         }
     }
 }
